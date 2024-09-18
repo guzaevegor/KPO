3fed570cf1aa08e399adcdf480ffaafb81103564
Egor-Guzaev <enotguzaev604@gmail.com>
2024-09-18 14:08:09 +0300

    solved 2. labs

--- lab2/task1.c
+++ lab2/task1.c
@@ -1,1 +1,67 @@
 /* Дана матрица размера M x N. Поменять местами столбцы, содержащие минимальный и максимальный элемент матрицы.  */
+#include <stdio.h>
+
+int main() {
+    int M, N;
+    
+    // Ввод размеров матрицы
+    printf("Input M: ");
+    scanf("%d", &M);
+    printf("Input N: ");
+    scanf("%d", &N);
+
+    int matrix[M][N];
+
+    // Ввод элементов матрицы
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("Input matrix[%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Вывод изначальной матрицы
+    printf("Original matrix:\n");
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Инициализация переменных для поиска минимального и максимального элемента
+    int min = matrix[0][0], max = matrix[0][0];
+    int min_col = 0, max_col = 0;
+
+    // Поиск столбцов с минимальным и максимальным элементами
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            if (matrix[i][j] < min) {
+                min = matrix[i][j];
+                min_col = j;
+            }
+            if (matrix[i][j] > max) {
+                max = matrix[i][j];
+                max_col = j;
+            }
+        }
+    }
+
+    // Обмен столбцов
+    for (int i = 0; i < M; i++) {
+        int temp = matrix[i][min_col];
+        matrix[i][min_col] = matrix[i][max_col];
+        matrix[i][max_col] = temp;
+    }
+
+    // Вывод измененной матрицы
+    printf("Modified matrix:\n");
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
--- lab2/task2.c
+++ lab2/task2.c
@@ -1,1 +1,57 @@
 /*Дана матрица размера M x N (M - четное число). Поменять местами верхнюю и нижнюю половину матрицы. */
+#include <stdio.h>
+
+int main() {
+    int M, N;
+
+    // Ввод размеров матрицы
+    printf("Input M (M should be an even number): ");
+    scanf("%d", &M);
+    printf("Input N: ");
+    scanf("%d", &N);
+
+    // Проверка на четность M
+    if (M % 2 != 0) {
+        printf("M must be an even number!\n");
+        return 1; // Завершаем программу, если M нечетное
+    }
+
+    int matrix[M][N];
+
+    // Ввод элементов матрицы
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("Input matrix[%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Вывод изначальной матрицы
+    printf("Original matrix:\n");
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Обмен верхней и нижней половины матрицы
+    for (int i = 0; i < M / 2; i++) {
+        for (int j = 0; j < N; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[M / 2 + i][j];
+            matrix[M / 2 + i][j] = temp;
+        }
+    }
+
+    // Вывод измененной матрицы
+    printf("Modified matrix (upper and lower halves swapped):\n");
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
