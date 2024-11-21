eec6e989c6ad3441f0c218acdf9ccbb8bb01eb25
Egor-Guzaev <enotguzaev604@gmail.com>
2024-11-21 23:29:27 +0300

    solved

--- lab7/task1.c
+++ lab7/task1.c
@@ -1,1 +1,66 @@
 /*Разработать программу, объединяющую несколько файлов содержащих списки студенческих групп, в один результирующий файл. Запись производится в алфавитном порядке*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINES 1000
+#define MAX_LENGTH 256
+
+void mergeFiles(char *outputFile, int fileCount, char *inputFiles[]) {
+    char *lines[MAX_LINES];
+    int lineCount = 0;
+
+    // Читаем строки из всех файлов
+    for (int i = 0; i < fileCount; i++) {
+        FILE *file = fopen(inputFiles[i], "r");
+        if (!file) {
+            perror("Ошибка открытия файла");
+            continue;
+        }
+
+        char buffer[MAX_LENGTH];
+        while (fgets(buffer, MAX_LENGTH, file)) {
+            // Удаляем символ новой строки
+            buffer[strcspn(buffer, "\n")] = '\0';
+            lines[lineCount] = strdup(buffer);
+            lineCount++;
+        }
+        fclose(file);
+    }
+
+    // Сортируем строки
+    for (int i = 0; i < lineCount - 1; i++) {
+        for (int j = i + 1; j < lineCount; j++) {
+            if (strcmp(lines[i], lines[j]) > 0) {
+                char *temp = lines[i];
+                lines[i] = lines[j];
+                lines[j] = temp;
+            }
+        }
+    }
+
+    // Записываем результат в выходной файл
+    FILE *output = fopen(outputFile, "w");
+    if (!output) {
+        perror("Ошибка открытия выходного файла");
+        return;
+    }
+
+    for (int i = 0; i < lineCount; i++) {
+        fprintf(output, "%s\n", lines[i]);
+        free(lines[i]); // Освобождаем память
+    }
+
+    fclose(output);
+    printf("Объединение завершено. Результат в файле %s.\n", outputFile);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Использование: %s output_file input_file1 input_file2 ...\n", argv[0]);
+        return 1;
+    }
+
+    mergeFiles(argv[1], argc - 2, &argv[2]);
+    return 0;
+}
--- lab7/task2.c
+++ lab7/task2.c
@@ -1,1 +1,64 @@
 /*Разработать программу слияниях двух отсортированных по убыванию значений элементов файлов F1 и F2. Результатом сияния должен быть файл F3, элементы которого упорядоченны по возрастанию*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINES 1000
+
+void mergeSortedFiles(const char *file1, const char *file2, const char *outputFile) {
+    int numbers[MAX_LINES];
+    int count = 0;
+
+    // Функция для чтения чисел из файла
+    void readNumbersFromFile(const char *filename) {
+        FILE *file = fopen(filename, "r");
+        if (!file) {
+            perror("Ошибка открытия файла");
+            return;
+        }
+
+        int num;
+        while (fscanf(file, "%d", &num) == 1) {
+            numbers[count++] = num;
+        }
+        fclose(file);
+    }
+
+    // Читаем числа из обоих файлов
+    readNumbersFromFile(file1);
+    readNumbersFromFile(file2);
+
+    // Сортировка массива по возрастанию
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = i + 1; j < count; j++) {
+            if (numbers[i] > numbers[j]) {
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+
+    // Запись результата в выходной файл
+    FILE *output = fopen(outputFile, "w");
+    if (!output) {
+        perror("Ошибка открытия выходного файла");
+        return;
+    }
+
+    for (int i = 0; i < count; i++) {
+        fprintf(output, "%d\n", numbers[i]);
+    }
+
+    fclose(output);
+    printf("Слияние завершено. Результат в файле %s.\n", outputFile);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Использование: %s file1 file2 output_file\n", argv[0]);
+        return 1;
+    }
+
+    mergeSortedFiles(argv[1], argv[2], argv[3]);
+    return 0;
+}
